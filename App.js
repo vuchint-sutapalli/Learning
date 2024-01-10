34153283524efc905f999aba5eaf213e4e41dd45
vuchint-sutapalli <vuchint.sutapalli@gmail.com>
2024-01-10 17:55:46 +0530

    installed parcel,react,react-dom through npm

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
+
+import React from "react";
+import { createRoot } from 'react-dom/client';
+
 const heading = React.createElement(
     "div",
     {"id": "he adi ng"},
-    [React.createElement("h1",{},"first heading"), React.createElement("h2",{},"second heading")]
+    [React.createElement("h1",{'key':'1'},"first heading"), React.createElement("h2",{'key':'2'},"second heading")]
     
     );
 
 console.log(heading)
 const rootElement = document.getElementById('root');
 
-ReactDOM.render(heading, rootElement);
+const root = createRoot(rootElement)
+
+root.render(heading);
